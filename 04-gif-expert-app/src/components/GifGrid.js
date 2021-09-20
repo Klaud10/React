000857860231f2e237e01284f873d42f3a4736a3
Claudio <claudio.falabella@gmail.com>
2021-09-20 08:03:14 +0100

    Video 79 completo

--- 04-gif-expert-app/src/components/GifGrid.js
+++ 04-gif-expert-app/src/components/GifGrid.js
@@ -1,35 +1,17 @@
-import React, { useState, useEffect } from 'react'
+import React from 'react'
+import { useFetchGifts } from '../hooks/useFetchGifs'
 import { GifGridItem } from './GifGridItem';
 
 export const GifGrid = ({category}) => {
 
-    const [images, setImages] = useState([]);
-
-    useEffect ( () => {
-        getGifs();
-    }, [])
-
-    const getGifs = async() => {
-
-        const url = `https://api.giphy.com/v1/gifs/search?q=${encodeURI(category)}&limit=10&api_key=WTtZZGTwxD3kTGfGMADGLLLuFW195bm4`;
-        const resp = await fetch (url); 
-        const {data} = await resp.json();
-
-        const gifs = data.map (img => {
-            return {
-                id: img.id,
-                title: img.title,
-                url: img.images?.downsized_medium.url,
-            }
-        }); 
-        
-        console.log(gifs);
-        setImages (gifs);
-    }; 
+    const {data:images, loading} = useFetchGifts(category);
 
     return (
         <>
             <h3> {category}</h3>
+
+            {loading && <p>Cargando...</p>}
+
             <div className="card-grid">
                 {
                     images.map( img => 
