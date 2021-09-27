faf9e2789f57ae9351fe1b359def49f1313b6496
Claudio Falabella <claudio.falabella@gmail.com>
2021-09-27 19:37:43 +0100

    CounterWitthCustomHok

---
+++ 05-hooks-app/src/components/01-useState/CounterWitthCustomHok.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { useCounter } from '../../hooks/useCounter';
+import './counter.css';
+
+export const CounterWitthCustomHok = () => {
+   
+    const { state, increment, decrement, reset } = useCounter(100);
+
+    return (
+        <div className="text-center">
+            <h1>Counter with Hook : { state }</h1>   
+            <hr />
+            <button onClick={ () => increment (2) } className="btn btn-success ms-2"> +1 </button>
+            <button onClick={ reset } className="btn btn-warning ms-2"> Reset </button>
+            <button onClick={ () => decrement (2) } className="btn btn-danger ms-2"> -1 </button>
+        </div>
+    )
+}
---
+++ 05-hooks-app/src/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+// import { CounterApp } from './components/01-useState/CounterApp';
+import { CounterWitthCustomHok } from './components/01-useState/CounterWitthCustomHok';
+// import { HooksApp } from './HooksApp';
+
+// ReactDOM.render(
+//     <HooksApp />,
+//   document.getElementById('root')
+// );
+
+
+ReactDOM.render(
+    <CounterWitthCustomHok />, 
+  document.getElementById('root')
+);
